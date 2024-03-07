5b410a6c7b94a469f977063fd455d2de164f05fd
Serkan Yılmaz <mainserkan@gmail.com>
2024-03-07 16:10:31 +0300

    fourth migration
    db connnection operations are successfull and first migration is added

---
+++ Infrastructure/SchoolProject.Persistence/CommandRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Domain.Application.Abstraction.Repository;
+using SchoolProject.Domain.Entities;
+
+namespace SchoolProject.Persistence
+{
+	public class CommandRepository<T> : ICommandRepository<T> where T : BaseEntity
+	{
+		public CommandRepository()
+		{
+		}
+
+        public DbSet<T> Table => throw new NotImplementedException();
+
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

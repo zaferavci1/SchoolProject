82c8bffde3c6bddd216388df8868becf6a310373
Zafer Avcı <93323232+zaferavci1@users.noreply.github.com>
2024-03-08 12:55:33 +0300

    Post, PublicProfile and User DTO's added

---
+++ Core/SchoolProject.Domain/Application/Features/PublicProfiles/DTOs/UpdatePublicProfileDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SchoolProject.Domain.Application.Abstraction.DTO;
+
+namespace SchoolProject.Domain.Application.Features.PublicProfiles.DTOs
+{
+	public class UpdatePublicProfileDTO : IDTO
+	{
+        public int Id { get; set; }
+        public string NickName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+    }
+}
+
--- Infrastructure/SchoolProject.Persistence/Repositories/Baskets/BasketCommandRepository.cs
+++ Infrastructure/SchoolProject.Persistence/Repositories/Baskets/BasketCommandRepository.cs
@@ -6,8 +6,14 @@ using SchoolProject.Persistence.Context;
 namespace SchoolProject.Persistence.Repositories.Baskets
 {
 	public class BasketCommandRepository : CommandRepository<Basket>, IBasketCommandRepository
+<<<<<<< Updated upstream
 	{
 		public BasketCommandRepository(SchoolProjectDbContext eSchoolProjectDbContext) : base(eSchoolProjectDbContext) { }
+=======
+    {
+		public BasketCommandRepository(SchoolProjectDbContext schoolProjectDbContext
+			) : base(schoolProjectDbContext) { }
+>>>>>>> Stashed changes
 	}
 }
 

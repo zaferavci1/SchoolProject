cdc693c370c510691f8cf0d0a5de17e85b928d0e
Zafer Avcı <93323232+zaferavci1@users.noreply.github.com>
2024-03-09 15:42:54 +0300

    Crpto, Post and PublicProfile Services added

---
+++ Core/SchoolProject.Application/Abstraction/Services/ICryptoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SchoolProject.Domain.Entities;
+
+namespace SchoolProject.Application.Abstraction.Services
+{
+	public interface ICryptoService
+	{
+		Task<Crypto> GetCryptoFromAPIAsync(string CryptoId);
+	}
+}
+
---
+++ Core/SchoolProject.Application/Abstraction/Services/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SchoolProject.Application.Features.Posts.DTOs;
+
+namespace SchoolProject.Application.Abstraction.Services
+{
+	public interface IPostService
+	{
+		Task<(List<GetAllPostsDTO>, int totalCount)> GetAllAsync(int page, int size);
+		Task<GetByIdPostDTO> GetByIdAsync(int id);
+		Task<PostDTO> AddAsync(AddPostDTO addPostDTO);
+		Task<PostDTO> UpdateAsync(UpdatePostDTO updatePostDTO);
+		Task<PostDTO> DeleteAsync(int id);
+	}
+}
+

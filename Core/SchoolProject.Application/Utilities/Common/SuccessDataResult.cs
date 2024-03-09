a8eefec63f691f12950480853cc7fc72a9971532
Zafer Avcı <93323232+zaferavci1@users.noreply.github.com>
2024-03-09 14:39:03 +0300

    Utilities are fixied

---
+++ Core/SchoolProject.Application/Utilities/Common/SuccessDataResult.cs
@@ -0,0 +1,12 @@
+﻿using SchoolProject.Application.Abstraction.DTO;
+
+namespace SchoolProject.Application.Utilities.Common
+{
+    public class SuccessDataResult<T> : DataResult<T> where T : class, IDTO, new()
+    {
+        public SuccessDataResult(string message, T data) : base(message, true, data) { }
+        public SuccessDataResult(T data) : base(true, data) { }
+        public SuccessDataResult(string message) : base(message, true, default) { }
+        public SuccessDataResult() : base(true, default) { }
+    }
+}

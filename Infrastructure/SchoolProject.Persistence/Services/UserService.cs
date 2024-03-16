ab8506c874d0cab3b0c1b3dfc6aa6606c2d8c88c
Serkan Yılmaz <mainserkan@gmail.com>
2024-03-16 16:03:46 +0300

    16.04.2024
    dataProtectors writed

--- Infrastructure/SchoolProject.Persistence/Services/UserService.cs
+++ Infrastructure/SchoolProject.Persistence/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using SchoolProject.Application.Abstraction.Repository.Users;
 using SchoolProject.Application.Abstraction.Services;
@@ -14,11 +15,17 @@ namespace SchoolProject.Persistence.Services
     {
         IUserCommandRepository _userCommandRepository;
         IUserQueryRepository _userQueryRepository;
+        private readonly IDataProtector userDataProtector;
+        private readonly IDataProtector postDataProtector;
+        private readonly IDataProtector commentDataProtector;
 
-        public UserService(IUserQueryRepository userQueryRepository, IUserCommandRepository userCommandRepository)
+        public UserService(IUserQueryRepository userQueryRepository, IUserCommandRepository userCommandRepository, IDataProtectionProvider dataProtectionProvider)
         {
             _userQueryRepository = userQueryRepository;
             _userCommandRepository = userCommandRepository;
+            userDataProtector = dataProtectionProvider.CreateProtector("Users");
+            postDataProtector = dataProtectionProvider.CreateProtector("Posts");
+            commentDataProtector = dataProtectionProvider.CreateProtector("Comments");
         }
 
         public async Task<UserDTO> AddAsync(AddUserDTO addUserDTO)
@@ -33,20 +40,38 @@ namespace SchoolProject.Persistence.Services
                 Password = addUserDTO.password
             });
             await _userCommandRepository.SaveAsync();
-            return new() { Id = Convert.ToString(user.Id), Mail = user.Mail, Name = user.Name, NickName = user.NickName, PhoneNumber = user.PhoneNumber, Surname = user.Surname, Password = user.Password };
+            return new()
+            {
+                Id = userDataProtector.Protect(user.Id.ToString()),
+                Mail = user.Mail,
+                Name = user.Name,
+                NickName = user.NickName,
+                PhoneNumber = user.PhoneNumber,
+                Surname = user.Surname,
+                Password = user.Password
+            };
         }
 
         public async Task<UserDTO> DeleteAsync(string id)
         {
-            User user = await _userCommandRepository.RemoveAsync(id);
+            User user = await _userCommandRepository.RemoveAsync(userDataProtector.Unprotect(id));
             await _userCommandRepository.SaveAsync();
-            return new() { Id = Convert.ToString(user.Id), Mail = user.Mail, Name = user.Name, Surname = user.Surname, NickName = user.NickName, PhoneNumber = user.PhoneNumber, Password = user.Password };
+            return new()
+            {
+                Id = userDataProtector.Protect(user.Id.ToString()),
+                Mail = user.Mail,
+                Name = user.Name,
+                Surname = user.Surname,
+                NickName = user.NickName,
+                PhoneNumber = user.PhoneNumber,
+                Password = user.Password
+            };
         }
 
         public async Task<(List<GetAllUsersDTO>, int totalCount)> GetAllAsync(int page, int size)
             => (await _userQueryRepository.GetAll().Where(u => u.IsActive == true).Skip(size * page).Take(size).Select(u => new GetAllUsersDTO
             {
-                Id = Convert.ToString(u.Id),
+                Id = userDataProtector.Protect(u.Id.ToString()),
                 Mail = u.Mail,
                 Name = u.Name,
                 Surname = u.Surname,
@@ -56,10 +81,10 @@ namespace SchoolProject.Persistence.Services
 
         public async Task<GetByIdUserDTO> GetByIdAsync(string id)
         {
-            User? user = await _userQueryRepository.Table.Include(u => u.Posts).ThenInclude(c => c.Comments).Include(u => u.Followers).Include(u => u.Follows).FirstOrDefaultAsync(u => u.Id == Guid.Parse(id));
+            User? user = await _userQueryRepository.Table.Include(u => u.Posts).ThenInclude(c => c.Comments).Include(u => u.Followers).Include(u => u.Follows).FirstOrDefaultAsync(u => u.Id == Guid.Parse(userDataProtector.Unprotect(id)));
             return new()
             {
-                Id = Convert.ToString(user.Id),
+                Id = userDataProtector.Protect(user.Id.ToString()),
                 Mail = user.Mail,
                 Name = user.Name,
                 Surname = user.Surname,
@@ -67,26 +92,27 @@ namespace SchoolProject.Persistence.Services
                 PhoneNumber = user.PhoneNumber,
                 Followers = user.Followers?.Select(f => new PublicProfilesDTO()
                 {
-                    Id = Convert.ToString(f.Id),
+                    Id = userDataProtector.Protect(f.Id.ToString()),
                     Name = f.Name,
                     Surname = f.Surname,
                     NickName = f.NickName
                 }).ToList() ?? new List<PublicProfilesDTO>(),
                 Follows = user.Follows?.Select(f => new PublicProfilesDTO()
                 {
-                    Id = Convert.ToString(f.Id),
+                    Id = userDataProtector.Protect(f.Id.ToString()),
                     Name = f.Name,
                     Surname = f.Surname,
                     NickName = f.NickName
                 }).ToList() ?? new List<PublicProfilesDTO>(),
                 Posts = user.Posts?.Select(p => new GetAllPostsDTO()
                 {
-                    Id = Convert.ToString(p.Id),
+                    Id = postDataProtector.Protect(p.Id.ToString()),
                     Content = p.Content,
                     Title = p.Title,
                     Comments = p.Comments.Select(c => new CommentDTO()
                     {
-                        Id = Convert.ToString(c.Id),
+                        PostId = postDataProtector.Protect(c.PostID.ToString()),
+                        Id = commentDataProtector.Protect(c.Id.ToString()),
                         Content = c.Content,
                     }).ToList() ?? new List<CommentDTO>()
                 }).ToList() ?? new List<GetAllPostsDTO>(),
@@ -95,7 +121,7 @@ namespace SchoolProject.Persistence.Services
 
         public async Task<UserDTO> UpdateAsync(UpdateUserDTO updateUserDTO)
         {
-            User user = await _userQueryRepository.GetByIdAsync(updateUserDTO.Id);
+            User user = await _userQueryRepository.GetByIdAsync(userDataProtector.Unprotect(updateUserDTO.Id));
             user.Name = updateUserDTO.Name == "string" ? user.Name : updateUserDTO.Name;
             user.Surname = updateUserDTO.Surname == "string" ? user.Surname : updateUserDTO.Surname;
             user.NickName = updateUserDTO.NickName == "string" ? user.NickName : updateUserDTO.NickName;
@@ -103,10 +129,11 @@ namespace SchoolProject.Persistence.Services
             user.IsActive = updateUserDTO.IsActive == default ? user.IsActive : updateUserDTO.IsActive;
             user.IsProfilePrivate = updateUserDTO.IsProfilePrivate == default ? user.IsProfilePrivate : updateUserDTO.IsProfilePrivate;
             user.Password = updateUserDTO.Password == "string" ? user.Password : updateUserDTO.Password;
+            _userCommandRepository.Update(user);
             await _userCommandRepository.SaveAsync();
             return new()
             {
-                Id = Convert.ToString(user.Id),
+                Id = userDataProtector.Protect(user.Id.ToString()),
                 Name = user.Name,
                 Surname = user.Surname,
                 NickName = user.NickName,
@@ -117,8 +144,8 @@ namespace SchoolProject.Persistence.Services
         }
         public async Task<UserDTO> FollowSomeoneAsync(string user1Id, string user2Id)
         {
-            User? user1 = await _userQueryRepository.Table.Include(u => u.Follows).FirstOrDefaultAsync(u => u.Id == Guid.Parse(user1Id));
-            User? user2 = await _userQueryRepository.Table.Include(u => u.Followers).FirstOrDefaultAsync(u => u.Id == Guid.Parse(user2Id));
+            User? user1 = await _userQueryRepository.Table.Include(u => u.Follows).FirstOrDefaultAsync(u => u.Id == Guid.Parse(userDataProtector.Unprotect(user1Id)));
+            User? user2 = await _userQueryRepository.Table.Include(u => u.Followers).FirstOrDefaultAsync(u => u.Id == Guid.Parse(userDataProtector.Unprotect(user2Id)));
             user1.Follows.Add(user2);
 
             user2.Followers.Add(user1);
@@ -128,7 +155,7 @@ namespace SchoolProject.Persistence.Services
             await _userCommandRepository.SaveAsync();
             return new()
             {
-                Id = Convert.ToString(user1.Id),
+                Id = userDataProtector.Protect(user1.Id.ToString()),
                 Name = user1.Name,
                 Surname = user1.Surname,
                 NickName = user1.NickName,

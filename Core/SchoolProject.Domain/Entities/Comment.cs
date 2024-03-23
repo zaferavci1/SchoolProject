52ae928df94508558ce67c5d7340a1bad2ba9e8d
Serkan Yılmaz <mainserkan@gmail.com>
2024-03-23 14:52:30 +0300

    commit 23.03
    some bugs are fixed

--- Core/SchoolProject.Domain/Entities/Comment.cs
+++ Core/SchoolProject.Domain/Entities/Comment.cs
@@ -3,8 +3,9 @@ namespace SchoolProject.Domain.Entities
 {
 	public class Comment : BaseEntity
     {
-        public Guid UserId { get; set; }
         public Guid PostID { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
         public List<Comment> ReplyComments { get; set; }
 		public string Content { get; set; }
 		public int LikeCount { get; set; }

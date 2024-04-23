703c0881c600c7043178d63a4c44cf97974e81d7
Zafer Avcı <93323232+zaferavci1@users.noreply.github.com>
2024-04-23 09:52:48 +0300

    Program cs updated
    Program cs updated

--- Infrastructure/SchoolProject.Persistence/obj/Debug/net7.0/SchoolProject.Persistence.AssemblyInfo.cs
+++ Infrastructure/SchoolProject.Persistence/obj/Debug/net7.0/SchoolProject.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolProject.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0776b6d3b91917b6cceb4d70a037bcece3c836")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolProject.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolProject.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
